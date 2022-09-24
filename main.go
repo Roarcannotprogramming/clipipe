6f93c65a3bd4c6006a089f13679278105d4ed512
v1me <z1991998920@gmail.com>
2022-09-24 14:00:11 +0800

    tmp

--- main.go
+++ main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
 
+	pb "github.com/Roarcannotprogramming/clipipe/proto"
+
+	"google.golang.org/grpc"
+)
+
+var (
+	port = flag.Int("port", 8799, "The server port")
+)
+
+type server struct {
+	pb.UnimplementedClipServer
+}
+
+func (s *server) Ping(ctx context.Context, in *pb.PingRequest) (*pb.PingResponse, error) {
+	return &pb.PingResponse{id: pb.PingRequest.id}, nil
+}
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterClipServer(s, &server{})
+	log.Printf("Server listening on port %d", *port)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
